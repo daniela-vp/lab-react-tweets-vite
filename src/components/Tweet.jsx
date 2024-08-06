9b3429bb2f17c8453cdb3a8ad0f5205b951882f9
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-08-06 18:28:59 +0100

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,25 +1,28 @@
-function Tweet() {
+
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+
+
+function Tweet(props) {
+
+
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.name} className="profile" alt="profile" />
+
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+        <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+        <Timestamp time={props.tweet.timestamp} />
+
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+        <Message message={props.tweet.message} />
         </p>
 
         <div className="actions">
